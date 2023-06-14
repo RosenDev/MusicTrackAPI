3f853e360b378228a500f53c677993f7caee1ca3
RosenDev <ros20021999@gmail.com>
2023-06-14 22:15:31 +0300

    merge branch development into main

--- MusicTrackAPI/Controllers/PlaylistsController.cs
+++ MusicTrackAPI/Controllers/PlaylistsController.cs
@@ -1,4 +1,13 @@
-﻿namespace MusicTrackAPI.Controllers
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using MusicTrackAPI.Commands.Playlist;
+using MusicTrackAPI.Data.Domain;
+using MusicTrackAPI.Model;
+using MusicTrackAPI.Model.Playlist;
+using MusicTrackAPI.Model.Track;
+using MusicTrackAPI.Services.Interface;
+
+namespace MusicTrackAPI.Controllers
 {
     public class PlaylistsController : ApiController<Playlist, PlaylistModel>
     {
